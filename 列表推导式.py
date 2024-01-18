080d2044971883a79f7b4ab01f828b68713fd025
marcelloarpad <124774781+marcelloarpad@users.noreply.github.com>
2024-01-18 22:09:05 +0800

    Add files via upload

---
+++ 列表推导式.py
@@ -0,0 +1,6 @@
+name1 = ['najs', 'sds', 'ffr']
+new_names = [name.upper()for name in name1]
+print(new_names)
+
+mnltiples = [i for i in range(30) if i % 3 == 0]
+print(mnltiples)
---
+++ 字典推导式.py
@@ -0,0 +1,3 @@
+listdemo = ['google', 'runoob', 'taobao']
+newdict = {key:len(newdict) for key in listdemo}
+print(newdict)
